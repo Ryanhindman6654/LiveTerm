114229e049ee36915474f9073d6c7a40ad162087
Ryan Hindman <45013407+Ryanhindman6654@users.noreply.github.com>
2024-03-12 10:27:01 -0700

    Update sumfetch.ts

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -39,7 +39,7 @@ const sumfetch = async (args: string[]): Promise<string> => {
 
                                             -----------
                                              Other Site
-                                             <u><a href="${config.main_site}" target="_blank">Main Site</a></u>
+                                             <u><a href="${config.main_site}" target="_blank">Portfolio Site</a></u>
                                              <u><a href="${config.status_site}" target="_blank">Status Page</a></u>
 `;
   }
